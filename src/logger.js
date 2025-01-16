191aa98a36f5d25a118640f163e6f9e880f05e4d
auppal12 <auppal12@myseneca.ca>
2025-01-16 00:59:43 -0500

    git url fixed

--- src/logger.js
+++ src/logger.js
@@ -4,7 +4,7 @@ const options = { level: process.env.LOG_LEVEL || 'info' };
 // If we're doing `debug` logging, make the logs easier to read
 if (options.level === 'debug') {
   // Log all environment variables when in debug mode
-  console.log('All Environment Variables:', process.env);
+  console.log(process.env, 'All Environment Variables:');
   // https://github.com/pinojs/pino-pretty
   options.transport = {
     target: 'pino-pretty',
