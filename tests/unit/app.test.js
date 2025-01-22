ce7efbd2fb6efada0927458f13098da0ff2721f6
auppal12 <auppal12@myseneca.ca>
2025-01-21 23:03:38 -0500

    unit tests setup to increase the test coverage of app.js and get.js

---
+++ tests/unit/app.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const app = require('../../src/app');
+
+describe('App', () => {
+    it('should return 404 for unknown routes', async () => {
+        const response = await request(app).get('/unknown-route');
+        expect(response.status).toBe(404);
+        expect(response.body).toEqual({
+            status: 'error',
+            error: {
+                message: 'not found',
+                code: 404,
+            },
+        });
+    });
+});
