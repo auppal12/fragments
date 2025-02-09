f48419f176c640c342ad9b15a862cbe5c1f08851
auppal12 <auppal12@myseneca.ca>
2025-02-08 22:25:21 -0500

    now returns fragments

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -1,12 +1,27 @@
-const { createSuccessResponse } = require("../../response");
+const { createSuccessResponse, createErrorResponse } = require("../../response");
+const { Fragment } = require('../../model/fragment');
+const logger = require('../../logger');
 
 /**
  * Get a list of fragments for the current user
  */
-module.exports = (req, res) => {
-    // TODO: this is just a placeholder. To get something working, return an empty array...
-    res.status(200).json({
-        ...createSuccessResponse(),
-        fragments: [],
-    });
+module.exports = async (req, res) => {
+    try {
+        logger.debug({ userId: req.user }, 'Getting fragments for user');
+        
+        // Get all fragments for this user
+        const fragments = await Fragment.byUser(req.user);
+        
+        logger.debug({ userId: req.user, fragments: fragments.length }, 'Got fragments');
+        
+        res.status(200).json({
+            ...createSuccessResponse(),
+            fragments,
+        });
+    } catch (error) {
+        logger.error({ error, userId: req.user }, 'Unable to get fragments for user');
+        res.status(500).json(
+            createErrorResponse(500, error.message)
+        );
+    }
 };
