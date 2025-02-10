148f36b339105423f1cef915f6bf59c12efbe2b4
auppal12 <auppal12@myseneca.ca>
2025-02-10 14:35:48 -0500

    "same"

--- tests/unit/app.test.js
+++ tests/unit/app.test.js
@@ -2,15 +2,15 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 describe('App', () => {
-    it('should return 404 for unknown routes', async () => {
-        const response = await request(app).get('/unknown-route');
-        expect(response.status).toBe(404);
-        expect(response.body).toEqual({
-            status: 'error',
-            error: {
-                message: 'not found',
-                code: 404,
-            },
-        });
+  it('should return 404 for unknown routes', async () => {
+    const response = await request(app).get('/unknown-route');
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({
+      status: 'error',
+      error: {
+        message: 'not found',
+        code: 404,
+      },
     });
+  });
 });
