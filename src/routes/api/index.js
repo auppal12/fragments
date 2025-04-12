adc69389318c2e64a90338be172d32a98385eee3
auppal12 <auppal12@myseneca.ca>
2025-04-12 16:57:22 -0400

    added PUT route

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -14,17 +14,20 @@ router.get('/fragments', require('./get'));
 
 // Support sending various Content-Types on the body up to 5M in size
 const rawBody = () =>
-    express.raw({
-        inflate: true,
-        limit: '5mb',
-        type: (req) => {
-            // See if we can parse this content type. If we can, `req.body` will be
-            // a Buffer (e.g., `Buffer.isBuffer(req.body) === true`). If not, `req.body`
-            // will be equal to an empty Object `{}` and `Buffer.isBuffer(req.body) === false`
-            const { type } = contentType.parse(req);
-            return Fragment.isSupportedType(type);
-        },
-    });
+  express.raw({
+    inflate: true,
+    limit: '5mb',
+    type: (req) => {
+      // See if we can parse this content type. If we can, `req.body` will be
+      // a Buffer (e.g., `Buffer.isBuffer(req.body) === true`). If not, `req.body`
+      // will be equal to an empty Object `{}` and `Buffer.isBuffer(req.body) === false`
+      const { type } = contentType.parse(req);
+      return Fragment.isSupportedType(type);
+    },
+  });
+
+// Use a raw body parser for PUT, which will give a `Buffer` Object or `{}` at `req.body`
+router.put('/fragments/:id', rawBody(), require('./put'));
 
 // Use a raw body parser for POST, which will give a `Buffer` Object or `{}` at `req.body`
 router.post('/fragments', rawBody(), require('./post'));
